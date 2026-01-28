0b124f362fbd851dbfebca470138b2ec733516b8
David Ospina <87675943+DaveOzz14@users.noreply.github.com>
2026-01-28 11:34:15 -0500

    Add files via upload
    Cambio Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import CreditForm from './pages/CreditForm';
 import CreditConfirmation from './pages/CreditConfirmation';
@@ -8,9 +9,17 @@ import PaymentError from './pages/PaymentError';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* Login */}
+      <Route path="/" element={<Login />} />
+
+      {/* Home */}
+      <Route path="/Home" element={<Home />} />
+
+      {/* Negocio Crédito */}
       <Route path="/credit" element={<CreditForm />} />
       <Route path="/credit/confirmation" element={<CreditConfirmation />} />
+
+      {/* Negocio Pago */}
       <Route path="/payment" element={<PaymentForm />} />
       <Route path="/payment/error" element={<PaymentError />} />
     </Routes>
